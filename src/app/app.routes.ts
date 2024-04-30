29e662c6b8279d666b208401754799005fbb537a
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-04-30 14:58:10 -0500

    Revert "feat: config routes, httpClient module added"
    This reverts commit 1902cb4de4ea88de10f23dc8185788fbf8fc5d17.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ForumComponent } from './pages/forum/forum.component';
-import {ThreadComponent} from "./pages/thread/thread.component";
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "forums", component: ForumComponent},
-    {path: "forums/:id", component: ThreadComponent}
+    {path: "forum/:id", component: ForumComponent}
 ];
