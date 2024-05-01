e865eb7a260bc55b931ffcd4a39f2f449daaabd8
Alexander Justo <102604349+AlexanderJusto@users.noreply.github.com>
2024-05-01 11:08:10 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ForumComponent } from './pages/forum/forum.component';
+import { PaymentComponent } from "./pages/payment/payment.component";
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "forum/:id", component: ForumComponent}
+    {path: "forum/:id", component: ForumComponent},
+    {path: "payment", Component: PaymentComponent}
 ];
