cea366a8a164c5b2d6287557f9d5f200964ecec6
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-05-01 17:54:23 -0500

    feat: analytics component without data added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ForumComponent } from './pages/forum/forum.component';
+import {AnalyticsComponent} from "./pages/analytics/analytics.component";
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "forum/:id", component: ForumComponent}
+    {path: "forum/:id", component: ForumComponent},
+    {path: "analytics", component: AnalyticsComponent}
 ];
