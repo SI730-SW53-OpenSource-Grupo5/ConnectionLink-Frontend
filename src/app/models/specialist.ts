6c7cccb7884d6a00ccd15b44d02de59bb01fccb2
Djalma Dioses <54141599+djalmadioses@users.noreply.github.com>
2024-05-01 07:31:22 -0500

    feat: add User and Specialist Model

---
+++ src/app/models/specialist.ts
@@ -0,0 +1,15 @@
+export interface Specialist {
+    "firstName": string,
+    "lastName": string,
+    "age": number,
+    "studies": string,
+    "ocupation": string,
+    "biography": string,
+    "cv": string,
+    "urlImage": string,
+    "username": string,
+    "email": string,
+    "password": string,
+    "etiquets": string,
+    "phone": string
+}
