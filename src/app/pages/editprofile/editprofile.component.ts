ef3d31b604781f276ddf73a81bbae1b400843716
Johan Príncipe Godoy <u202014511@upc.edu.pe>
2024-06-02 19:22:33 -0500

    feat: edit profile changes

--- src/app/pages/editprofile/editprofile.component.ts
+++ src/app/pages/editprofile/editprofile.component.ts
@@ -1,51 +1,138 @@
-import { Component, OnInit } from '@angular/core';
-import {FormsModule, NgForm} from '@angular/forms';
+/*
+import {Component, EventEmitter, OnInit, ViewChild} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
-import {MatFormField} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import {ActivatedRoute, Router} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-editprofile',
   standalone: true,
-  imports: [ MatFormField, FormsModule, MatInput],
+  imports: [MatFormFieldModule, FormsModule, MatInputModule, NgIf],
   templateUrl: './editprofile.component.html',
   styleUrls: ['./editprofile.component.scss']
 })
 export class EditprofileComponent implements OnInit {
+  @ViewChild('userForm', { static: false })
   userForm!: NgForm;
   userData!: User;
-  id: string='1';
-  constructor(private userService: UserService) {}
+  showPopup: boolean = false;
+  editeProfile!: boolean;
+
+  clickedPopup = new EventEmitter<void>();
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {
     this.userData = {} as User;
-    this.getUser(this.id);
+    this.getUser();
+    this.editeProfile = true;
   }
 
-  getUser(id: string) {
-    this.userService.getUserById(id).subscribe(
-      (response: User) => {
-        console.log('User data', response);
-        this.userData = response ;
+  updateProfile() {
+    this.showPopup = !this.showPopup;
+  }
+
+  goToPage(page: string): void {
+    this.router.navigate([page]);
+  }
+
+  getUser() {
+    this.userService.getUser('asmith').subscribe(
+      (response: any) => {
+        console.log('User fetched successfully:', response);
+        this.userData = response[0];
+      },
+      (error: any) => {
+        console.error('Error fetching user:', error);
       }
     );
   }
 
   onSubmit() {
-    if(this.userForm.form.value.toString().length > 0) {
-      this.userService.updateUser(this.id, this.userForm.value.userData).subscribe(
-        (response: User) => {
-          console.log('User updated', response);
-        },
-        (error: any) => {
-          console.log('Error updating user', error);
-        }
-      );
-    }
+    console.log('Form submitted:', this.userData);
+    this.updateProfile();
+    /!*if(this.userForm.valid) {
+      this.userService.update(this.userData).subscribe(
+      (response: any) => {
+        console.log('User updated successfully:', response);
+      },
+      (error: any) => {
+        console.error('Error updating user:', error);
+      }
+    );
+   }*!/
   }
 
   onCancel() {
     console.log('Form canceled');
   }
 }
+*/
+
+import { Component, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {CommonModule} from "@angular/common";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-editprofile',
+  standalone: true,
+  imports: [MatFormFieldModule, FormsModule, MatInputModule,  CommonModule],
+  templateUrl: './editprofile.component.html',
+  styleUrls: ['./editprofile.component.scss']
+})
+export class EditprofileComponent implements OnInit {
+  @ViewChild('userForm', { static: false }) userForm!: NgForm;
+  @Output() clickedPopup = new EventEmitter<void>();
+  userData!: User;
+  showPopup = true; // or false depending on when you want to show the popup
+  editProfile = false;
+
+  constructor(private router: Router, private userService: UserService) {}
+
+  ngOnInit() {
+    this.userData = {} as User;
+    this.getUser();
+  }
+
+  getUser() {
+    this.userService.getUser('asmith').subscribe(
+      (response: any) => {
+        console.log('User fetched successfully:', response);
+        this.userData = response[0];
+      },
+      (error: any) => {
+        console.error('Error fetching user:', error);
+      }
+    );
+  }
+
+  onSubmit() {
+    this.editProfile = true;
+    /*if (this.userForm.valid) {
+      this.userService.updateUser(this.userData).subscribe(
+        (response: any) => {
+          console.log('User updated successfully:', response);
+          this.editProfile = true;
+        },
+        (error: any) => {
+          console.error('Error updating user:', error);
+        }
+      );
+    } else {
+      console.error('Form is invalid');
+    }*/
+  }
+
+  goToPage(page: string): void {
+    this.router.navigate([page]);
+  }
+}
+
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {catchError, Observable, throwError} from 'rxjs';
+import {catchError, Observable, retry, throwError} from 'rxjs';
 import { User } from '../models/user';
 import {environment} from "../enviroments/environments";
 
@@ -11,20 +11,35 @@ export class UserService {
   base_Url=`${environment.baseUrl}/users`;
   constructor(private http: HttpClient) {}
 
-  getUser(username: string): Observable<User> {
-    return this.http.get<User>(this.base_Url + "/?username=" + username);
-  }
-  getUserById(id: string): Observable<any> {
-    return this.http.get<User>(`${this.base_Url}/${id}`);
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    })
   }
 
-  updateUser(id:string, data: User): Observable<User> {
-    return this.http.put<User>(`${this.base_Url}/${id}`, data).pipe(
-      catchError((error: any)=> {
-        console.error('Error updating user', error);
-        return throwError(() => new Error('Error updating user'));
-      })
-    );
+  handleError(error: HttpErrorResponse) {
+    // Default error handling
+    if (error.error instanceof ErrorEvent) {
+      console.log(`An error occurred ${error.error.message}`);
+    } else {
+      // Unsuccessful Response Error Code returned from Backend
+      console.log(`Backend returned code ${error.status}, body was ${error.error}`);
+    }
+    return throwError(() => new Error('Something happened with request, please try again later'));
+  }
+
+  getUser(username: string): Observable<User> {
+    return this.http.get<User>(this.base_Url + "/?username=" + username)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+  getUserById(id: string): Observable<any> {
+    return this.http.get<User>(`${this.base_Url}/${id}`)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  updateUser(data: User): Observable<User> {
+    return this.http.put<User>(this.base_Url + "/?username=" + data.username, JSON.stringify(data), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
   }
 
 }
